37c19702bc8947fd1c66d104f8a60029a8ea738f
Danting Huang <serena.huangdanting@gmail.com>
2022-05-04 23:47:43 -0400

    working on controllers and models

--- server/models/index.js
+++ server/models/index.js
@@ -2,14 +2,51 @@ var db = require('../db');
 
 module.exports = {
   messages: {
-    get: function () {}, // a function which produces all the messages
-    post: function () {} // a function which can be used to insert a message into the database
+    get: function (callback) {
+      db.dbConnection.query('SELECT * FROM messages', function (err, results) {
+        if (err) {
+          throw err;
+        } else {
+          callback(results);
+        }
+      });
+    }, // a function which produces all the messages
+    post: function (message, callback) {
+      var queryString = `INSERT INTO messages VALUES (null, ${message.text}, ${message.username}, ${message.roomname})`;
+      db.dbConnection.query(queryString, function(err, results) {
+        if (err) {
+          throw err;
+        } else {
+          callback(results);
+        }
+
+      });
+    } // a function which can be used to insert a message into the database
+
   },
 
   users: {
     // Ditto as above.
-    get: function () {},
-    post: function () {}
+    get: function (callback) {
+      db.dbConnection.query('SELECT * FROM users', function (err, results) {
+        if (err) {
+          throw err;
+        } else {
+          callback(results);
+        }
+      });
+    },
+    post: function (message, callback) { //?????may need another look
+      var queryString = `INSERT INTO users VALUES (null,${message.username}`;
+      db.dbConnection.query(queryString, function(err, results) {
+        if (err) {
+          throw err;
+        } else {
+          callback(results);
+        }
+
+      });
+    }
   }
 };
 
